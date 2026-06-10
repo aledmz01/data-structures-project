e24efdb993f49b0a05d55749aca48a65be1a1f22
DMurgas <105890876+DMurgas@users.noreply.github.com>
2026-06-09 18:09:36 -0600

    Add files via upload

---
+++ Models/NodoAVL.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace proyectEstructura.Models
+{
+    public class NodoAVL
+    {
+        public string Palabra;
+
+        public NodoAVL Izquierdo;
+        public NodoAVL Derecho;
+
+        public int Altura;
+
+        public NodoAVL(string palabra)
+        {
+            Palabra = palabra;
+            Altura = 1;
+        }
+    }
+}
